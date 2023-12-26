41729cdd01a750b5886402dc25b2f3db7f75e3b1
Amit Kumar <amit11dec90@gmail.com>
2023-12-26 23:33:21 +0530

    (server): resolve the cors issues

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,10 @@ import cartRoutes from "./routes/carts";
 import categoryRoutes from "./routes/categories";
 import orderRoutes from "./routes/orders";
 import authRoutes from "./routes/auth";
+import cors from "cors";
+
+// Bypass SSL certificate validation (not recommended in production)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 dotenv.config({ path: "./.env" });
 
@@ -15,6 +19,21 @@ dotenv.config({ path: "./.env" });
 connectDB();
 
 const app = express();
+// Enable CORS for all routes
+app.use(cors());
+
+// OR specify options for CORS
+// Replace allowedOrigins with your client's URL or '*' to allow all origins (not recommended in production)
+const corsOptions = {
+  //origin: "http://localhost:3000/", // Replace with your client's URL
+  //origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //credentials: true, // Enable CORS credentials if required (e.g., cookies, authorization headers)
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+};
+
+app.use(cors(corsOptions));
+
 const port = process.env.PORT;
 
 app.use(express.json());
