d1c23cb029d27acb81ab51d27ba37c36923fb2d4
Amit Kumar <amit11dec90@gmail.com>
2024-01-22 00:03:35 +0530

    ui fix cart

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,11 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import {
-  fetchCartItems,
   updateCartItem,
   removeFromCart,
 } from "../appState/actions/cartActions";
 import { Link, useNavigate } from "react-router-dom";
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import { Button, Container, Image, Table } from "react-bootstrap";
 
 const CartPage = () => {
   const navigate = useNavigate();
@@ -14,10 +13,6 @@ const CartPage = () => {
   const { cartItems = [], totalAmount } = useSelector(
     (state: any) => state.cart.cartItems
   );
-
-  // const state = useSelector((state: any) => state);
-
-  // console.log("hey cart state", state);
 
   const handleUpdateCart = async (itemId, quantity) => {
     await dispatch(updateCartItem(userId, itemId, quantity));
@@ -30,9 +25,6 @@ const CartPage = () => {
   const handleCheckout = () => {
     isAuthenticated ? navigate("/checkout") : navigate("/login");
   };
-
-  // const getTotalPrice = () =>
-  //   cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
 
   if (cartItems.length === 0) {
     return (
@@ -51,68 +43,81 @@ const CartPage = () => {
 
   return (
     <Container className="mt-4">
-      <Row>
-        <Col md={6}>
-          <h3>Cart</h3>
+      <h5 className="text-center mb-4">Your Cart ({cartItems.length} items)</h5>
+      <Table bordered responsive>
+        <thead>
+          <tr>
+            <th className="align-middle text-center">Item</th>
+            <th className="align-middle text-center">Price</th>
+            <th className="align-middle text-center">Quantity</th>
+            <th className="align-middle text-center">Total</th>
+            <th className="align-middle text-center"> Remove</th>
+          </tr>
+        </thead>
+        <tbody>
           {cartItems.map((item) => (
-            <div
-              data-testid={`cart-item-${item.productId}`}
-              key={item.productId}
-              className="mb-4 border rounded p-3"
-            >
-              <Row>
-                <Col md={4}>
-                  <Image src={item.image} alt={item.name} fluid />
-                </Col>
-                <Col md={8}>
-                  <h5>{item.name}</h5>
-                  <p>Price: ${item.price}</p>
-                  <div className="d-flex align-items-center">
-                    <Button
-                      variant="outline-primary"
-                      size="sm"
-                      className="me-2"
-                      onClick={() =>
-                        handleUpdateCart(item.productId, item.quantity + 1)
-                      }
-                    >
-                      +
-                    </Button>
-                    <span>{item.quantity}</span>
-                    <Button
-                      variant="outline-primary"
-                      size="sm"
-                      className="ms-2"
-                      onClick={() =>
-                        handleUpdateCart(item.productId, item.quantity - 1)
-                      }
-                    >
-                      -
-                    </Button>
-                    <Button
-                      variant="outline-danger"
-                      size="sm"
-                      className="ms-auto"
-                      onClick={() => handleRemoveFromCart(item.productId)}
-                    >
-                      Remove
-                    </Button>
-                  </div>
-                </Col>
-              </Row>
-            </div>
+            <tr key={item.productId}>
+              <td className="align-middle text-center">
+                <Image
+                  style={{ width: "80", height: "80px" }}
+                  src={item.image}
+                  alt={item.name}
+                />
+                <div className="mt-2">{item.name}</div>
+              </td>
+              <td className="align-middle text-center">₹{item.price}</td>
+              <td className="align-middle  text-center">
+                <div className="d-flex justify-content-center align-items-center">
+                  <Button
+                    variant="outline-primary"
+                    size="sm"
+                    className="me-2"
+                    onClick={() =>
+                      handleUpdateCart(item.productId, item.quantity + 1)
+                    }
+                  >
+                    +
+                  </Button>
+                  <span>{item.quantity}</span>
+                  <Button
+                    variant="outline-primary"
+                    size="sm"
+                    className="ms-2"
+                    onClick={() =>
+                      handleUpdateCart(item.productId, item.quantity - 1)
+                    }
+                  >
+                    -
+                  </Button>
+                </div>
+              </td>
+              <td className="align-middle text-center">
+                ₹{item.price * item.quantity}
+              </td>
+              <td className="align-middle text-center">
+                <Button
+                  variant="link"
+                  className="text-danger"
+                  onClick={() => handleRemoveFromCart(item.productId)}
+                >
+                  {"x"}
+                </Button>
+              </td>
+            </tr>
           ))}
-        </Col>
-        <Col md={6}>
-          <h3>Price Details</h3>
+        </tbody>
+      </Table>
+
+      <div className="mt-4">
+        <h5 className="text-center mb-4">Price Details</h5>
+        <div className="border p-4">
           <p>
-            Price ({cartItems.length} items): ${totalAmount}
+            Price ({cartItems.length} items): ₹{totalAmount}
           </p>
-          {/* Display discount, delivery charges, etc. */}
-          <p>Discount: $0.00</p>
-          <p>Delivery Charges: $0.00</p>
+          <p>Discount: ₹0.00</p>
+          <p>Delivery Charges: ₹0.00</p>
           <hr />
-          <p>Total Price: ${totalAmount}</p>
+          <p>Total Price: ₹{totalAmount}</p>
           <Button
             variant="primary"
             onClick={handleCheckout}
@@ -121,8 +126,8 @@ const CartPage = () => {
           >
             Checkout
           </Button>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </Container>
   );
 };
